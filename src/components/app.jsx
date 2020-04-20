8854900265bcf0f18b63519ba17a01445bca7b81
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-04-20 18:49:50 +0000

    added components to view, review, and create cards, started defining methods in app.jsx and state to manage views

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import ViewCards from './view-cards';
+import ReviewCards from './review-cards';
+import CreateCard from './create-card';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state{
+      view: 'view-cards'
+    }
+  }
+  setView(view) {
+    this.setState({ view: view })
+  }
+  getView() {
+    switch (this.state.view) {
+      case 'create-card':
+        return <CreateCard />;
+      case 'review-cards':
+        return <ReviewCards />;
+      case 'view-cards':
+        return <ViewCards />;
+      default:
+        return null;
+    }
+  }
   render() {
     return <h1 className="text-center">Flash Card App</h1>;
   }
